c29538352759c533f9fc3249a87bd39a82be430a
Jiehong Brandon Chung <jbx.chung@gmail.com>
2019-10-01 12:12:55 -0400

    restyle containing iframe

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-1. 'npm start' in root directory runs the bot on localhost:3000
-2. 'npm run watch" in ./ext-app runs the extension app on localhost:4000
+1. open https://develop2.symphony.com/client/index.html?bundle=https://localhost:4000/bundle.json
+2. 'npm start' in root directory runs the bot on localhost:3000
+3. 'npm run watch' in ./ext-app runs the extension app on localhost:4000
--- ext-app/src/js/controller.js
+++ ext-app/src/js/controller.js
@@ -12,7 +12,7 @@ SYMPHONY.remote.hello()
         render(e, data) {
             console.log('CitiRfq: rendering ', data, e);
             return {
-                template: '<messageML><div style="border-radius: 4px; width: 800px; overflow: hidden;background: border: 1px solid #838995;box-sizing: border-box;box-shadow: 0px 1px 10px #007ECC;"><iframe src="https://localhost:4000/rfq.html" width="100%" height="305px" /></div></messageML>',
+                template: '<messageML><div style="width: 100%; overflow: hidden;border: 1px solid #838995;box-sizing: border-box;"><iframe src="https://localhost:4000/rfq.html" width="100%" height="305px" /></div></messageML>',
                 data: {}
               };
         }
