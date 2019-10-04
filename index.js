e1a02ce92c03bd90bc6bb5a67288bba0c5a16f75
Jiehong Brandon Chung <jbx.chung@gmail.com>
2019-10-04 12:05:19 -0400

    add regex to parse rfq object

--- index.js
+++ index.js
@@ -2,11 +2,42 @@ const Symphony = require('symphony-api-client-node');
 Symphony.setDebugMode(true)
 require('./server.js');
 
+// temporary until we plug in nlp
+const getRfqFromMessageObject = (message) => {
+  let { messageText } = message;
+
+  const REGEX = {
+    DIRECTION: /(buy|sell)/ig,
+    PRICE: /(at|@)( +)?\d+/ig,
+    SIZE: /(\d+)?\.?\d+(k|m|b){0,2}/ig,
+  };
+
+  const directionMatch = messageText.match(REGEX.DIRECTION);
+  if (directionMatch) { messageText = messageText.replace(directionMatch[0], ''); }
+  const priceMatch = messageText.match(REGEX.PRICE);
+  if (priceMatch) { messageText = messageText.replace(priceMatch[0], ''); }
+  const sizeMatch = messageText.match(REGEX.SIZE);
+  if (sizeMatch) { messageText = messageText.replace(sizeMatch[0], ''); }
+
+  const rfq = {
+    direction: directionMatch ? directionMatch[0] : '',
+    price: priceMatch ? priceMatch[0].replace(/\D/ig, '') : '',
+    size: sizeMatch ? sizeMatch[0] : '',
+  };
+  // just use whatever's left for the description
+  rfq.description = messageText.trim();
+
+  return rfq;
+}
+
+// keep track of rfqs every time we restart the bot
+let prevRfqId = 0;
 const botHearsSomething = (event, messages) => {
   messages.forEach((message, index) => {
     let reply_message = 'Hello ' + message.user.firstName + ', hope you are doing well!!'
     reply_message += '<span class="entity" data-entity-id="summary"></span>';
     // TODO: turn the message text into data here (e.g. call NLP)
+    const rfq = getRfqFromMessageObject(message);
 
     // set data to render into the "summary" entity span defined above
     //symphony ext app will render "com.citi.rfq" to iframe loading rfq ui by the rfqId;
@@ -16,11 +47,8 @@ const botHearsSomething = (event, messages) => {
         version: '0.1',
         message,
         payload: {
-          rfqId: 'xxxx',
-          description: 'bond name',
-          direction: 'buy',
-          size: '123',
-          price: '123',
+          rfqId: prevRfqId++,
+          ...rfq,
         },
       },
     };
@@ -36,4 +64,4 @@ const botHearsSomething = (event, messages) => {
 
 Symphony.initBot(__dirname + '/config.json').then((symAuth) => {
   Symphony.getDatafeedEventsService(botHearsSomething);
-})
+});
