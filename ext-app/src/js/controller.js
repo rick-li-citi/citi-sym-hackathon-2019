919ab4e7d4c3114f2d28c7bd5d9b31da321ddd59
Rick Li <rick.li@citi.com>
2019-10-16 10:06:41 -0400

    fix app registeration

--- ext-app/src/js/controller.js
+++ ext-app/src/js/controller.js
@@ -11,16 +11,47 @@ let appTokenPromise = fetch(`${baseUrl}/appToken`)
 Promise.all([appTokenPromise, SYMPHONY.remote.hello()]).then((data) => {
   console.log('CitiRfq: hello done', data);
   return SYMPHONY.application.register(
-    "citi-rfq", 
-    ["modules", "applications-nav", "ui", "share", "entity"], 
+    {appId: "citi-rfq", tokenA: appToken}, 
+    ["modules", "applications-nav", "ui", "share", "entity","extended-user-info"], 
     ["CitiRfq:controller"],
   );
  }).then((response) => {
+  const extendedUserInfoService = SYMPHONY.services.subscribe('extended-user-info');
+  const uiService = SYMPHONY.services.subscribe('ui');
+  const rfqButton = {
+    // icon: `${baseUrl}/img/icon_small.png`,
+    label: 'Rates RFQ',
+    data: {}
+  };
+  uiService.registerExtension('single-user-im', 'CitiRfq-im', 'CitiRfq:controller', rfqButton);
+  uiService.registerExtension('rooms', 'CitiRfq-im', 'CitiRfq:controller', rfqButton);
+  
+
+
+  let jwt = undefined;
+  let currentUser = undefined;
+  
+  // .then(jwt => {
+  //   console.log('jwt: ', jwt);
+  // });
+  extendedUserInfoService.getJwt().then(jwt => {
+    console.log('jwt: ', jwt);
+  });
+  // extendedUserInfoService.getEmail().then(email => {
+  //   console.log('email: ', email);
+  //     currentUser = email;
+  // });
+
   console.log('CitiRfq: subscribed modules.');
   CitiRfqService.implement({
+    trigger(uiClass, id, payload, data){
+      console.log(payload)
+    },
     render(e, data) {
       console.log('CitiRfq: rendering ', data, e);
-
+      extendedUserInfoService.getJwt().then(jwt => {
+        console.log('jwt: ', jwt);
+      });
       const jsonData = JSON.stringify(data);
       const iframeCss = `
         width: 100%;
@@ -40,13 +71,13 @@ Promise.all([appTokenPromise, SYMPHONY.remote.hello()]).then((data) => {
       };
     }
   });
-  let entityService = SYMPHONY.services.subscribe("entity");
-  entityService.registerRenderer(
-    "com.citi.rfq",
-    {},
-    "CitiRfq:controller",
-  );
+  // let entityService = SYMPHONY.services.subscribe("entity");
+  // entityService.registerRenderer(
+  //   "com.citi.rfq",
+  //   {},
+  //   "CitiRfq:controller",
+  // );
 })
-// .fail((e) => {
+// .catch((e) => {
 //   console.error(`Fail to register application `, e);
 // });
